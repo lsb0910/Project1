8531083e319e376df5649f49c39e06df892765ce
LeeSaeByeol <agsb0910@gmail.com>
2023-06-08 14:52:15 +0900

    커밋 2-2

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 # 함수 선언부
 def add_func(n1, n2) :
-    return = n1 + n2
+    return  n1 + n2
 
 def sub_func(n1, n2) :
-    return = n1 - n2
+    return  n1 - n2
 
 # 전역 변수부
 num1, num2, res =100, 200, 0
