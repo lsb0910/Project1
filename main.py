fe67b0cf9ad3f504f8d01006780eb7f121827e40
LeeSaeByeol <agsb0910@gmail.com>
2023-06-08 14:58:10 +0900

    커밋4 나누기 함수...

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ def sub_func(n1, n2) :
 def mul_func(n1, n2) :
     return  n1 * n2
 
+def div_func(n1, n2) :
+    return  n1 * n2
+
+
 # 전역 변수부
 num1, num2, res =100, 200, 0
 
@@ -21,4 +25,8 @@ print(num1, '-', num2, '=', res)
 
 res = mul_func(num1, num2)
 print(num1, '*', num2, '=', res)
+
+res = div_func(num1, num2)
+print(num1, '/', num2, '=', res)
+
 
